313cac72bb4fb8655d64a5cc49aab2a90a8a24af
NIKO PAULSON <niko@NIKOs-MacBook-Pro-2.local>
2019-11-29 18:51:07 -0500

    start of main function

---
+++ R/coord_cartesian_custom.R
@@ -0,0 +1,13 @@
+draw_axis <- function(line_sides,
+                      tick_sides,
+                      text_sides,
+                      breaks,
+                      minor_breaks,
+                      minor_tick_length,
+                      labels,
+                      axis_position,
+                      panel_params,
+                      theme) {
+
+  if (length(breaks) == 0) return(zeroGrob())
+}
